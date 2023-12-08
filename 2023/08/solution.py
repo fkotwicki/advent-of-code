afd40efcb68cbed44cb101d61cf4de19e4a77081
Filip Kotwicki <filip.kotwicki@gmail.com>
2023-12-08 15:34:46 +0100

    2023, Day 8 part 2

--- 2023/08/solution.py
+++ 2023/08/solution.py
@@ -1,3 +1,4 @@
+import math
 input = open('input.txt').read().split('\n\n')
 
 def part_one(input):
@@ -26,4 +27,38 @@ def part_one(input):
 
     return total_instructions
 
+def part_two(input):
+    instructions = input[0]
+    nodes = {}
+    start_nodes = []
+    for node_line in input[1].split('\n'):
+        node_parts = node_line.split('=')
+        node_next_elements = node_parts[1].strip()[1:-1].split(',')
+        node_key = node_parts[0].strip()
+        node_value = (node_next_elements[0].strip(), node_next_elements[1].strip())
+        nodes[node_key] = node_value
+        if node_key.endswith('A'):
+            start_nodes.append((node_value))
+    
+    total = []
+    for start_node in start_nodes:
+        current_instruction = 0
+        current_node = start_node
+        total_instructions = 1
+        while True:
+            if current_instruction == len(instructions):
+                current_instruction = 0
+
+            current_node = current_node[0 if instructions[current_instruction] == 'L' else 1]
+            if current_node.endswith('Z'):
+                break
+        
+            current_node = nodes[current_node]
+            current_instruction += 1
+            total_instructions += 1
+        total.append(total_instructions)
+
+    return math.lcm(*total)
+
 print(f'Part one: {part_one(input)}')
+print(f'Part two: {part_two(input)}')
